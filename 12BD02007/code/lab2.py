435386ddc23de8e1eba94ec8504b147f4173e954
unknown <sergey.gordeev94@gmail.com>
2014-10-02 20:34:51 +0600

    lab2

---
+++ 12BD02007/code/lab2.py
@@ -0,0 +1,199 @@
+﻿import http.server
+import socketserver
+import re
+from cgi import parse_header, parse_multipart
+from urllib.parse import parse_qs
+
+from model import *
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
+
+class Controller:
+	def __init__(self):
+		self.sl = ServiceLayer()
+		self.fileName = None
+		self.action = None
+		self.param  = None
+		self.textExt = ['.html', '.htm', '.foot', '.head']
+
+	def getMethodByAction(self, action):
+		try:
+			return {'index':'getPosts', 'insertPost':'insertPost'}[action]
+		except:
+			return action
+		
+	def buildPage(self, fileName, action=None, param=None):
+		if len([i for i in self.textExt if i == fileName[fileName.rfind('.'):]]) == 0:
+			return self.readBFile(fileName)
+	
+		self.fileName = fileName
+		self.action = action
+		self.param  = param
+	
+		act = action
+		if act is None:
+			at = fileName.rfind('/')
+			if at == -1: at = 0
+			act = fileName[at:]
+			
+			at = fileName.rfind('.')
+			if at == -1: at = 0
+			act = act[:at]
+			
+			
+		methodName = self.getMethodByAction(act)
+		method = getattr(self.sl, methodName, None)
+		if method is not None:
+			method(param)
+	
+		text = self.readFile(self.fileName)
+		text = self.replaceKeyWords(text)
+		return bytes(text, 'UTF-8')
+			
+			
+	def replaceKeyWords(self, text):
+		text = self.replaceIteration(text)
+	
+		atA = text.find('<@')
+		atB = -1
+
+		while atA != -1:
+			atB = text.find('@>', atA)
+			hk = self.execute(text[atA+2:atB])
+			hk = self.replaceKeyWords(hk)
+			text = text[:atA] + hk + text[atB+2:]
+			atA = text.find('<@')
+
+		return text
+		
+	def replaceIteration(self, text):
+		p = re.compile('(<@\s*replicate\s*@>)(.*)(<@\s*replicate[ ]\s*@>)', re.DOTALL)
+		
+		m = p.search(text)
+		
+		while m is not None:
+			at = m.span(2)
+			atWhole = m.span()
+			p2 = re.compile('(<@\s*\w+\s*)(\w+)(\s*@>)')
+			cmd = text[at[0]:at[1]]
+			
+			hkC = ''
+			
+			for i in range(self.sl.getDataLength()):
+				hk = cmd
+				m2 = p2.search(hk)
+				
+				while m2 is not None:
+					at2 = m2.span(2)
+					atWhole2 = m2.span()
+
+					hk = hk[:atWhole2[0]] + self.sl.getValueOfAttr(hk[at2[0]:at2[1]], i) + hk[atWhole2[1]:]
+					m2 = p2.search(hk)
+				hkC += hk
+					
+			text = text[:atWhole[0]] + hkC + text[atWhole[1]:]
+			m = p.search(text)
+					
+		return text
+
+	def execute(self, command):
+		atA = command.find('include')
+		if atA > -1:
+			path = command[atA+7:].strip()
+			ans = self.readFile(path)
+			return ans
+			
+		atA = command.find('put')
+		if atA > -1:
+			attr = command[atA+3:].strip()
+			return self.sl.getValueOfAttr(attr)
+
+		return ''
+
+	def readFile(self, fileName):
+		try:
+			file = open(fileName, 'r')
+			lines = file.readlines()
+
+			fileText = ''
+			for line in lines:
+				fileText += line
+
+			at = fileText.find('<')
+			at = 0 if at == -1 else at
+			fileText = fileText[at:]			
+			
+			return fileText
+		except:
+			return ''
+		
+	def readBFile(self, fileName):
+		try:
+			with open(fileName, mode='rb') as file:
+				fileContent = file.read()
+			return fileContent
+		except FileNotFoundError as fnf:
+			print('Exception:', fnf)
+			return b''
+		
+
+
+class Handler(http.server.BaseHTTPRequestHandler):
+	cntrl = Controller()
+	
+	def do_HEAD(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+
+	def getFileName(self):
+		fileName = None
+
+		if self.path.strip() == '/':
+			fileName = 'index.html'
+		else:
+			fileName = self.path[1:]
+
+		return fileName
+		
+	def do_GET(self):
+		fileName = self.getFileName()
+			
+		page = self.cntrl.buildPage(fileName)
+		
+		self.do_HEAD()
+		self.wfile.write(page)
+
+	def parse_POST(self):
+		ctype, pdict = parse_header(self.headers['content-type'])
+		if ctype == 'multipart/form-data':
+			postvars = parse_multipart(self.rfile, pdict)
+		elif ctype == 'application/x-www-form-urlencoded':
+			length = int(self.headers['content-length'])
+			postvars = parse_qs(
+					self.rfile.read(length), 
+					keep_blank_values=1)
+		else:
+			postvars = {}
+		return postvars
+		
+		
+	def do_POST(self):
+		postvars = self.parse_POST()
+
+		fileName = self.getFileName()
+		page = self.cntrl.buildPage(fileName, None,postvars)
+		
+		self.do_HEAD()
+		self.wfile.write(page)
+		
+
+if __name__ == '__main__':
+	httpd = http.server.HTTPServer((HOST_NAME, PORT_NUMBER), Handler)
+	try:
+		print("Server Starting - %s:%s" % (HOST_NAME, PORT_NUMBER))
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		httpd.server_close()
+	print("Server Stopped - %s:%s" % (HOST_NAME, PORT_NUMBER))
---
+++ 12BD02007/code/model.py
@@ -0,0 +1,62 @@
+﻿import sqlite3
+from datetime import date
+
+class Post:
+	def __init__(self):
+		self.user = None
+		self.postText = None
+		self.date = None
+		
+	def __init__(self, user, postText, date):
+		self.user = user
+		self.postText = postText
+		self.date = date
+		
+	def __str__(self):
+		return self.user + " " + self.postText + " " + self.date
+		
+		
+class ServiceLayer:
+	def __init__(self):
+		self.data = None
+		self.conn = sqlite3.connect('posts.db')
+			
+	def getValueOfAttr(self, attrName, cnt=None):
+		return getattr(self.data if cnt is None else self.data[cnt], attrName, '')
+
+	def getDataLength(self):
+		return len(self.data)
+		
+	def getPosts(self, params = None):
+		listOfPosts = []
+	
+		cur = self.conn.cursor()
+
+		#rs = cur.execute('''delete from posts''')
+		#self.conn.commit()
+		
+		rs = cur.execute('''select username, post, date from posts order by date''')
+		
+		for row in rs:
+			listOfPosts.append(Post(row[0], row[1], row[2]))
+		
+		self.data = listOfPosts
+
+		
+	def insertPost(self, params):
+		if params is None:
+			return
+		
+		cur = self.conn.cursor()
+		today = date.today()
+		todayStr = str(today.year) + "-" + str(today.month) + "-" + str(today.day)
+
+		prm = [params[b'user'][0].decode('UTF-8'), params[b'textPost'][0].decode('UTF-8'), todayStr]
+
+		rs = cur.execute('''insert into posts(username, post, date) values(?, ?, ?)''', prm)
+		self.conn.commit()
+		
+	def __del__(self):
+		self.conn.commit()
+		self.conn.close()	
+		
